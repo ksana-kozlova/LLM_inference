e4677ba93c99e26b870e39e2db4fce14713b9a3d
ksana-kozlova <52532061+ksana-kozlova@users.noreply.github.com>
2023-12-10 11:49:52 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# LLM compression
+
+The project demonstrates the work of LLM and the acceleration of inference using various optimization methods such as Graph Optimization, kv-caching.
+
+To demonstrate the work, an application was made on Flask.
+To run, you need to install the dependencies from the file requirements.txt and launch the application.
+
+````
+python -m pip install -r requirements.txt
+flask --app app run
+````
---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify, render_template, session
+from flask_session import Session
+from datetime import datetime
+from neural_network.model import run_model
+
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+def generate_answer1(question):
+    return run_model(question)
+
+def generate_answer2(question):
+    return run_model(question)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        question_type = request.form.get("type")
+        question = request.form["question"]
+
+        # Выбор правильной истории на основе типа запроса
+        history_key = "history1" if question_type == "question1" else "history2"
+
+        if history_key not in session:
+            session[history_key] = []
+        session[history_key].append(question)
+
+        start_time = datetime.now()
+        
+        # Выбор функции генерации ответа
+        if question_type == "question1":
+            answer = generate_answer1(question)
+        elif question_type == "question2":
+            answer = generate_answer2(question)
+        else:
+            answer = "Неизвестный тип вопроса"
+
+        end_time = datetime.now()
+        response_time = (end_time - start_time).total_seconds()
+
+        return jsonify({"answer": answer, "response_time": response_time})
+    else:
+        return render_template("question.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
